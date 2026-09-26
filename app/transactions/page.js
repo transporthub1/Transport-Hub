705f6afe7f2c68a79fb2617c198004e9f353a180
Fakhar Abbas <infinitypages786@gmail.com>
2026-09-26 08:58:16 -0700

    Fix transaction history date time

--- app/transactions/page.js
+++ app/transactions/page.js
@@ -18,6 +18,13 @@ function normalizePhone(value) {
   return phone;
 }
 
+function formatTransactionDate(value) {
+  if (!value) return "N/A";
+  const date = new Date(value);
+  if (Number.isNaN(date.getTime())) return String(value);
+  return date.toLocaleString("en-GB", { day: "2-digit", month: "2-digit", year: "numeric", hour: "2-digit", minute: "2-digit", hour12: true });
+}
+
 function getTransactionDate(transaction) {
   return (
     transaction?.date ||
